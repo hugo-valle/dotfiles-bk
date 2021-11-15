631e2e349ce78b78ff398ce14c5232e4e5ce37dc
Hugo Valle <hugo.valle@gmail.com>
2021-11-15 12:23:36 -0700

    Change TAB key mapping in nvim-cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -15,17 +15,17 @@ local cmp = require'cmp'
       end,
     },
     mapping = {
-      ["<C-j>"] = cmp.mapping.select_next_item(),
-      ["<C-k>"] = cmp.mapping.select_prev_item(),
+      --["<C-j>"] = cmp.mapping.select_next_item(),
+      --["<C-k>"] = cmp.mapping.select_prev_item(),
       ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-      ['<TAB>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<Shift-TAB>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
       ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
       ['<C-e>'] = cmp.mapping({
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
       }),
-      --['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
